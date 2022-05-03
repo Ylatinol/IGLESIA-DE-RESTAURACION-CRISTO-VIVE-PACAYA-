03bedf582f77630114ef9b57c4788ea3da61e8f4
Ylatinol <95261864+Ylatinol@users.noreply.github.com>
2022-05-02 21:44:25 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Apr 30, 2022 19:00:00").getTime();
+let launchDate = new Date("may 05, 2022 18:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
